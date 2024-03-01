97c2cb695ee9d9f408ac6a2e2ce0f017568010fa
Silas Henrique <silash.silva00@gmail.com>
2024-02-29 23:42:53 -0300

    style!: add global structure styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,94 @@
+import GlobalStyles from "./GlobalStyles";
+
+const bg =
+  "https://images.unsplash.com/photo-1517971071642-34a2d3ecc9cd?q=80&w=1588&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <main>
+      <GlobalStyles themeName="coolGrey" />
+      <header
+        style={{
+          backgroundImage: `url('${bg}')`,
+        }}
+      >
+        <div className="typewriter">
+          <h1>O que fazer hoje?</h1>
+        </div>
+        <form>
+          <input type="text" placeholder="Correr, Estudar..." />
+          <button type="submit" aria-label="Adicionar novo item">
+            +
+          </button>
+        </form>
+      </header>
+
+      <section>
+        <form>
+          <input type="text" placeholder="Filtrar lista atual, ex: Dentista" />
+        </form>
+
+        <table border={1}>
+          <thead>
+            <tr>
+              <th align="left">
+                <input type="checkbox" disabled />
+              </th>
+              <th align="left">Id</th>
+              <th align="left">Conteúdo</th>
+              <th />
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>
+                <input type="checkbox" />
+              </td>
+              <td>d4f26</td>
+              <td>
+                Conteúdo de uma TODO Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Eaque vero facilis obcaecati, autem aliquid
+                eius! Consequatur eaque doloribus laudantium soluta optio odit,
+                provident, ab voluptates doloremque voluptas recusandae
+                aspernatur aperiam.
+              </td>
+              <td align="right">
+                <button data-type="delete">Apagar</button>
+              </td>
+            </tr>
+
+            <tr>
+              <td colSpan={4} align="center" style={{ textAlign: "center" }}>
+                Carregando...
+              </td>
+            </tr>
+
+            <tr>
+              <td colSpan={4} align="center">
+                Nenhum item encontrado
+              </td>
+            </tr>
+
+            <tr>
+              <td colSpan={4} align="center" style={{ textAlign: "center" }}>
+                <button data-type="load-more">
+                  Carregar mais{" "}
+                  <span
+                    style={{
+                      display: "inline-block",
+                      marginLeft: "4px",
+                      fontSize: "1.2em",
+                    }}
+                  >
+                    ↓
+                  </span>
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    </main>
+  );
 }
