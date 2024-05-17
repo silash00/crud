efd3ec055b87caa851565af7d5b374012fa5712c
Silas Henrique <silash.silva00@gmail.com>
2024-05-16 22:18:59 -0300

    chore: add relative paths

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import GlobalStyles from "./GlobalStyles";
+import GlobalStyles from "@ui/theme/GlobalStyles";
 
 const bg =
   "https://images.unsplash.com/photo-1517971071642-34a2d3ecc9cd?q=80&w=1588&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
